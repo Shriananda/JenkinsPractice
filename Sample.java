3c0a76ac9aa0de4b63bdeb130d9f1e98a7c2a149
Shriananda <shriananda.s05@gmail.com>
2022-05-19 11:27:10 +0530

    Create Sample.java

---
+++ Sample.java
@@ -0,0 +1,10 @@
+public class Sample {
+
+ public static void main(String[] args) {
+  // TODO Auto-generated method stub
+    System.out.println("Hello Agni");
+	  System.out.println("Hello Riya");
+   System.out.println("Hello Abhi");
+ }
+
+}
