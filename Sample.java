8f3ac8ad23639151f102bdf8c5a4bb6008e7c2e4
Shriananda <shriananda.s05@gmail.com>
2022-05-19 13:04:55 +0530

    a3

--- Sample.java
+++ Sample.java
@@ -3,7 +3,8 @@ public class Sample {
  public static void main(String[] args) {
   // TODO Auto-generated method stub
     System.out.println("Hello Agni");
-	  System.out.println("Hello Riyas1");
+  System.out.println("Hello Riyas1");
+	 System.out.println("Hello Riyas1");
    System.out.println("Hello Abhi");
 	 System.out.println("Hello Jeyasudha");
 	  System.out.println("Hello Ghaghana");
